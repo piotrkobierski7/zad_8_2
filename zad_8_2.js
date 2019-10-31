00d875b676ad203b8120ae3f22fb62a7f83a7bd1
Piotr Kobierski <34033397+piotrkobierski7@users.noreply.github.com>
2019-10-31 18:53:45 +0100

    Correction of errors v1

--- zad_8_2.js
+++ zad_8_2.js
@@ -1,56 +1,43 @@
 'use strict';
-
 var output = document.getElementById('output');
-
 output.innerHTML = 'Click the button!' + '<br><br>' + output.innerHTML; 
-
 var button_CtoF = document.getElementById('C-F');
 var button_FtoC = document.getElementById('F-C');
-
-var temperatureC,temperatureF;
-
-function cTempIntoF(temperatureC){
+var tempOffsetFahr = 32; //static numbers for C->F formula
+var tempMultipleFahr = 1.8; //static numbers for C->F formula
+function cTempIntoF (temperatureC) {
 	var temperatureF;
-	return temperatureF=1.8*temperatureC+32;
+	return temperatureF=tempMultipleFahr*temperatureC+tempOffsetFahr;
 }
-
-function checkInfo(temperatureC){
-  	if(temperatureC < 0){ 
+function checkInfo (temperatureC) {
+  	if (temperatureC < 0){ 
     	return ' At this temperature water freezes';
-	}else if(temperatureC < 100){
+	} else if (temperatureC < 100) {
   		return ' At this temperature water is in liquid form';
-	}else{ 
+	} else { 
   		return ' At this temperature water is in gas form';
   	}
 }
-
-function fTempIntoC(temperatureF){
+function fTempIntoC (temperatureF) {
 	var temperatureC;
-	return temperatureC=(temperatureF - 32)/1.8;
+	return temperatureC = (temperatureF - tempOffsetFahr)/tempMultipleFahr;
 }
-
-
-function showInfo(typeOfDegree, temperature){
+function showInfo (typeOfDegree, temperature) {
 	var tempC;
-	if (!isNaN(temperature)) {
-		tempC=typeOfDegree === 'C' ? temperature : fTempIntoC(temperature);
-		output.innerHTML = 'Temperature in Celsius degrees is '+
-		tempC
-		+ '<br><br>' + 'Temperature in Fahrenheit degrees is '+
-		cTempIntoF(tempC)
-		+ '<br><br>' + checkInfo(tempC) + '<br><br>'+ '***************'  + '<br><br>' + output.innerHTML;
+	if (!isNaN (temperature)) {
+		tempC = typeOfDegree === 'C' ? temperature : fTempIntoC(temperature);
+		output.innerHTML = 'Temperature in Celsius degrees is '+ tempC
+		+ '<br><br>' + 'Temperature in Fahrenheit degrees is '+ cTempIntoF (tempC)
+		+ '<br><br>' + checkInfo (tempC) + '<br><br>'+ '***************'  + '<br><br>' + output.innerHTML;
 	} else {
     output.innerHTML = temperature +' is not a number'+ '<br><br>'+ output.innerHTML;
  }
 }
-
-
-button_FtoC.addEventListener('click', function() {
+button_FtoC.addEventListener ('click', function() {
 	var temperature = window.prompt('What is the temperature in Fahrenheit degree?');
   	showInfo('F',temperature);
 });
-
-button_CtoF.addEventListener('click', function() {
+button_CtoF.addEventListener ('click', function() {
 	var temperature = window.prompt('What is the temperature in Celsius degree?');
   	showInfo('C',temperature);
 });
